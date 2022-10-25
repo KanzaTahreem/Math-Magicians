c880cc948722a68a3c6ba89f709f1b44f8ea596d
KanzaTahreem <106119105+KanzaTahreem@users.noreply.github.com>
2022-10-26 04:43:02 +0500

    Use class based component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
+import React from 'react';
 import Calculator from './components/Calculator';
 import './stylesheets/Calculator.css';
 
-function App() {
-  return (
-    <Calculator />
-  );
+// eslint-disable-next-line react/prefer-stateless-function
+class App extends React.Component {
+  render() {
+    return <Calculator />;
+  }
 }
 
 export default App;
