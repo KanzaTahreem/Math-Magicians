8877d920b71d65c56e194be14b42fd516253e3ac
KanzaTahreem <106119105+KanzaTahreem@users.noreply.github.com>
2022-10-26 12:57:20 +0500

    Use CSS Modules to add styles

--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -1,44 +1,44 @@
 import React from 'react';
-import '../stylesheets/Calculator.css';
+import styles from '../stylesheets/Calculator.module.css';
 
 // eslint-disable-next-line react/prefer-stateless-function
 class Calculator extends React.Component {
   render() {
     return (
-      <section className="calculator">
-        <div className="container">
-          <div className="display">
+      <section className={styles.calculator}>
+        <div className={styles.container}>
+          <div className={styles.display}>
             <p>0</p>
           </div>
-          <div className="keys">
+          <div className={styles.keys}>
             <div>
-              <button type="button" className="btn">AC</button>
-              <button type="button" className="btn">+/-</button>
-              <button type="button" className="btn">%</button>
-              <button type="button" className="btn btn-right">/</button>
+              <button type="button" className={styles.btn}>AC</button>
+              <button type="button" className={styles.btn}>+/-</button>
+              <button type="button" className={styles.btn}>%</button>
+              <button type="button" className={`${styles.btn} ${styles['btn-right']}`}>/</button>
             </div>
             <div>
-              <button type="button" className="btn">7</button>
-              <button type="button" className="btn">8</button>
-              <button type="button" className="btn">9</button>
-              <button type="button" className="btn btn-right">x</button>
+              <button type="button" className={styles.btn}>7</button>
+              <button type="button" className={styles.btn}>8</button>
+              <button type="button" className={styles.btn}>9</button>
+              <button type="button" className={`${styles.btn} ${styles['btn-right']}`}>x</button>
             </div>
             <div>
-              <button type="button" className="btn">4</button>
-              <button type="button" className="btn">5</button>
-              <button type="button" className="btn">6</button>
-              <button type="button" className="btn btn-right">-</button>
+              <button type="button" className={styles.btn}>4</button>
+              <button type="button" className={styles.btn}>5</button>
+              <button type="button" className={styles.btn}>6</button>
+              <button type="button" className={`${styles.btn} ${styles['btn-right']}`}>-</button>
             </div>
             <div>
-              <button type="button" className="btn">1</button>
-              <button type="button" className="btn">2</button>
-              <button type="button" className="btn">3</button>
-              <button type="button" className="btn btn-right">+</button>
+              <button type="button" className={styles.btn}>1</button>
+              <button type="button" className={styles.btn}>2</button>
+              <button type="button" className={styles.btn}>3</button>
+              <button type="button" className={`${styles.btn} ${styles['btn-right']}`}>+</button>
             </div>
             <div>
-              <button type="button" className="btn btn-expand">0</button>
-              <button type="button" className="btn">.</button>
-              <button type="button" className="btn btn-right">=</button>
+              <button type="button" className={`${styles.btn} ${styles['btn-expand']}`}>0</button>
+              <button type="button" className={styles.btn}>.</button>
+              <button type="button" className={`${styles.btn} ${styles['btn-right']}`}>=</button>
             </div>
           </div>
         </div>
