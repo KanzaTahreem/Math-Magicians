e4c6ed2c45fd5c347c4913df7145f1bf0cd3bd18
KanzaTahreem <106119105+KanzaTahreem@users.noreply.github.com>
2022-10-27 10:09:11 +0500

    Add event handler to the calculator buttons

--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -30,33 +30,33 @@ class Calculator extends React.Component {
           </div>
           <div className={styles.keys}>
             <div>
-              <button type="button" className={styles.btn}>AC</button>
-              <button type="button" className={styles.btn}>+/-</button>
-              <button type="button" className={styles.btn}>%</button>
-              <button type="button" className={`${styles.btn} ${styles['btn-right']}`}>/</button>
+              <button type="button" className={styles.btn} onClick={this.handleClick}>AC</button>
+              <button type="button" className={styles.btn} onClick={this.handleClick}>+/-</button>
+              <button type="button" className={styles.btn} onClick={this.handleClick}>%</button>
+              <button type="button" className={`${styles.btn} ${styles['btn-right']}`} onClick={this.handleClick}>/</button>
             </div>
             <div>
-              <button type="button" className={styles.btn}>7</button>
-              <button type="button" className={styles.btn}>8</button>
-              <button type="button" className={styles.btn}>9</button>
-              <button type="button" className={`${styles.btn} ${styles['btn-right']}`}>x</button>
+              <button type="button" className={styles.btn} onClick={this.handleClick}>7</button>
+              <button type="button" className={styles.btn} onClick={this.handleClick}>8</button>
+              <button type="button" className={styles.btn} onClick={this.handleClick}>9</button>
+              <button type="button" className={`${styles.btn} ${styles['btn-right']}`} onClick={this.handleClick}>x</button>
             </div>
             <div>
-              <button type="button" className={styles.btn}>4</button>
-              <button type="button" className={styles.btn}>5</button>
-              <button type="button" className={styles.btn}>6</button>
-              <button type="button" className={`${styles.btn} ${styles['btn-right']}`}>-</button>
+              <button type="button" className={styles.btn} onClick={this.handleClick}>4</button>
+              <button type="button" className={styles.btn} onClick={this.handleClick}>5</button>
+              <button type="button" className={styles.btn} onClick={this.handleClick}>6</button>
+              <button type="button" className={`${styles.btn} ${styles['btn-right']}`} onClick={this.handleClick}>-</button>
             </div>
             <div>
-              <button type="button" className={styles.btn}>1</button>
-              <button type="button" className={styles.btn}>2</button>
-              <button type="button" className={styles.btn}>3</button>
-              <button type="button" className={`${styles.btn} ${styles['btn-right']}`}>+</button>
+              <button type="button" className={styles.btn} onClick={this.handleClick}>1</button>
+              <button type="button" className={styles.btn} onClick={this.handleClick}>2</button>
+              <button type="button" className={styles.btn} onClick={this.handleClick}>3</button>
+              <button type="button" className={`${styles.btn} ${styles['btn-right']}`} onClick={this.handleClick}>+</button>
             </div>
             <div>
-              <button type="button" className={`${styles.btn} ${styles['btn-expand']}`}>0</button>
-              <button type="button" className={styles.btn}>.</button>
-              <button type="button" className={`${styles.btn} ${styles['btn-right']}`}>=</button>
+              <button type="button" className={`${styles.btn} ${styles['btn-expand']}`} onClick={this.handleClick}>0</button>
+              <button type="button" className={styles.btn} onClick={this.handleClick}>.</button>
+              <button type="button" className={`${styles.btn} ${styles['btn-right']}`} onClick={this.handleClick}>=</button>
             </div>
           </div>
         </div>
