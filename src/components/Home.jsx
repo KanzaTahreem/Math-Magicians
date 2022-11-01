3a0c2e443c25f6422d8c1135782fa48ad80973da
KanzaTahreem <106119105+KanzaTahreem@users.noreply.github.com>
2022-11-01 21:31:36 +0500

    Update home page layout

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,27 +1,51 @@
 import React from 'react';
+import { ExternalLink } from 'react-external-link';
+import '../stylesheets/Home.css';
+import gif from '../images/math.gif';
 
 const Home = () => (
-  <section className="main-container">
-    <div className="description">
+  <section className="home-container">
+    <div>
       <h2 className="subtitle">Welcome to our page!</h2>
-      <p className="text">
-        <b><i>Math magicians</i></b>
-        is a website for all fans of mathematics.
-        It is a Single Page App (SPA) that allows users to:
-      </p>
-      <ul>
-        <li className="listItem">Make simple calculations.</li>
-        <li className="listItem">Read a random math-related quote.</li>
-      </ul>
-      <p>
-        The app is developed using a
-        <i>React</i>
-        environment, and styled through React basic styling.
-      </p>
-      <button type="button" className="source-code"> Source Code </button>
-      <button type="button" className="live-link">View Live</button>
+      <div className="description">
+        <p className="text">
+          <b>Math magicians</b>
+          {' '}
+          is a website for all fans of mathematics.
+          <br />
+          It is a Single Page App (SPA) that allows users to:
+        </p>
+        <ul>
+          <li className="listItem">Make simple calculations.</li>
+          <li className="listItem">Read a random math-related quote.</li>
+        </ul>
+        <p>
+          The app is developed using a
+          {' '}
+          <i>React,</i>
+          {' '}
+          and styled through
+          {' '}
+          <i>React basic styling.</i>
+          {' '}
+          Navigation is made possible through
+          {' '}
+          <i>React Router.</i>
+        </p>
+      </div>
+
+      <ExternalLink href="https://github.com/KanzaTahreem/Math-Magicians" className="external-link">
+        Source Code
+      </ExternalLink>
+
+      <ExternalLink href="https://kanzatahreem.github.io/Math-Magicians/" className="external-link">
+        View Live
+      </ExternalLink>
+
     </div>
-    <div className="img" />
+    <div className="home-img">
+      <img src={gif} alt="math" />
+    </div>
   </section>
 );
 
